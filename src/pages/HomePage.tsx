90a3bbea84debb35b39266a1a45315a2f5c8383a
youssef houmas <92693238+ysfzxz7@users.noreply.github.com>
2025-07-17 17:53:01 +0100

    add the hero section

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,11 +1,13 @@
-import { HeroSection } from '@/components/home/HeroSection';
-import { PopularHouses } from '@/components/home/PopularHouses';
-import { Features } from '@/components/home/Features';
-import { Testimonials } from '@/components/home/Testimonials';
+import { HeroSection } from "@/components/home/HeroSection";
+import { PopularHouses } from "@/components/home/PopularHouses";
+import { Features } from "@/components/home/Features";
+import { Testimonials } from "@/components/home/Testimonials";
+import Hero from "./Hero";
 
 export function HomePage() {
   return (
     <>
+      <Hero />
       <HeroSection />
       <PopularHouses />
       <Features />
